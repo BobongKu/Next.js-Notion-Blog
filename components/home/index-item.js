c2ca3fbe1e0ff2db0f1206e7cc43371f0f068f00
DevBO <42234678+BobongKu@users.noreply.github.com>
2023-11-13 06:07:27 +0900

    fix index recent projects

--- components/home/index-item.js
+++ components/home/index-item.js
@@ -9,7 +9,7 @@ export default function IndexItem({data,dir}) {
               </div>
               <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 py-5 m-6 gap-x-12 gap-y-16">
                 {data.results.map((data, index) => (
-                  <ProjectItem key={index} data={data} dir={dir}/>
+                  <ProjectItem key={index} data={data} dir={`${dir}s`}/>
                 ))}
               </div>
               <div className="flex justify-content-end hover:font-bold underline underline-offset-4">
