7e8ba666418516974b4c49427fa8acf927b5bb01
DevBO <42234678+BobongKu@users.noreply.github.com>
2023-11-13 05:06:25 +0900

    add GoToTop Button

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,11 @@
 import Footer from "./footer";
 import Header from "./header";
+import ScrollToTop from "./scrollToTop";
 
 export default function Layout({ children }) {
     return (
         <div className="bg-primary">
+            <ScrollToTop/>
             <Header/>
             <div>{children}</div>
             <Footer/>
---
+++ components/scrollToTop.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react"
+import Animation from "./animation";
+import lottieJson from "../public/scrollToTop.json"
+
+export default function ScrollToTop(){
+    const [showButton, setShowButton] = useState(false);
+
+    const scrollToTop = () => {
+        window.scroll({
+            top: 0,
+            behavior: 'smooth'
+        })
+
+    }
+    useEffect(() => {
+        const handleShowButton = () => {
+            if (window.scrollY > 500) {
+                setShowButton(true)
+            } else {
+                setShowButton(false)
+            }
+        }
+
+        console.log(window.scrollY)
+        window.addEventListener("scroll", handleShowButton)
+        return () => {
+            window.removeEventListener("scroll", handleShowButton)
+        }
+    }, [])
+
+    return showButton &&(
+        <div className="fixed z-30 right-10 bottom-8">
+            <button className="w-12 bg-gray-600 bg-opacity-20 border-2 border-opacity-10 border-white rounded-full 
+                            hover:border-gray-600 hover:border-opacity-60 hover:bg-opacity-60 dark:hover:border-white dark:hover:border-opacity-60 dark:hover:bg-opacity-60 dark:bg-white dark:bg-opacity-40" onClick={scrollToTop} type="button" >
+                <Animation lottieJson={lottieJson}/>
+            </button>
+        </div>
+    )
+}
