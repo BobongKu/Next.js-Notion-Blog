8f4f1013e505875ee0594f66725f9a0625e38a7f
DevBO <jhbgjy7@gmail.com>
2023-11-14 12:37:06 +0900

    Revert "lottie"
    This reverts commit fd88ca8128a4dd16c4800719448e92b098d4b7cf.

--- components/dark-mode-toggle-button.js
+++ components/dark-mode-toggle-button.js
@@ -7,7 +7,7 @@ export default function DarkModeToggleButton(){
         <>
             <button className="h-8 w-11 items-center bg-gray-200 border-0 py-1 px-3 rounded text-base mt-4 md:mt-0 focus:outline-none hover:bg-yellow-50 hover:text-orange-500
                 dark:hover:text-yellow-300 dark:bg-slate-700 dark:text-slate-400 dark:hover:bg-slate-800"
-                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} type="button" aria-label="theme button"
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} type="button"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="visible dark:invisible dark:h-0  dark:w-0 h-5 w-5">
                     <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
