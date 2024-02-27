2c6ccfcfdd4b90c39ac1523cd337c6f5481a0b9f
DevBO <jhbgjy7@gmail.com>
2024-02-27 15:41:46 +0900

    dd

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,16 @@
 import Footer from "./footer";
 import Header from "./header";
 import ScrollToTop from "./scrollToTop";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Layout({ children }) {
     return (
         <div className="bg-primary">
             <ScrollToTop/>
             <Header/>
-            <div>{children}</div>
+            <div>{children}
+            <Analytics />
+            </div>
             <Footer/>
         </div>
     )
